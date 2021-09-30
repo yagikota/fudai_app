58261ca9acc55baacbcc7ce0f7b79b2dac87da25
八木洸太 <yagikota@mbp.local>
2021-09-30 22:59:29 +0900

    カスタムユーザーにした#54

--- fudaichatproject/fudaichatapp/models.py
+++ fudaichatproject/fudaichatapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-
-# Create your models here.
+class User(AbstractUser):
+    pass
 
 class Question(models.Model):
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
